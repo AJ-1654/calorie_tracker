88a6a6b5c5f483e3490695a75e75edf3c12bf356
Anish <anish.jangra1654@outlook.com>
2020-04-28 13:03:18 +0530

    dev work complete

--- calorietracker/main/forms.py
+++ calorietracker/main/forms.py
@@ -4,6 +4,8 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
 
+
 class CalculateCalorie(FlaskForm):
-    picture = FileField('Update Profile Picture', validators=[
+    picture = FileField('Add food item to check calorie', validators=[
                         FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Check')
--- calorietracker/main/routes.py
+++ calorietracker/main/routes.py
@@ -1,21 +1,13 @@
-from flask import Blueprint
-from flask import render_template, request, Blueprint
-# from calorietracker.models import Post
+from flask import render_template, request, Blueprint, redirect, url_for
 from calorietracker.main.forms import (CalculateCalorie)
 from calorietracker.main.utils import save_picture
 
 main = Blueprint('main', __name__)
 
+
 @main.route("/")
-@main.route("/home",methods=['GET', 'POST'])
+@main.route("/home", methods=['GET'])
 def home():
-    form  = CalculateCalorie()
-    if form.validate_on_submit():
-        if form.picture.data:
-            picture_file = save_picture(form.picture.data)
-            # predict picture using model
-
-
     page = request.args.get('page', 1, type=int)
     return render_template('home.html')
 
@@ -24,3 +16,17 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
+@main.route("/check_calorie", methods=['GET', 'POST'])
+def check_calorie():
+    form = CalculateCalorie()
+    p_class = None
+    p_img = None
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            # predict picture using ml model
+            p_class = 'Pizza'
+            p_img = url_for('static', filename='food_pics/' + picture_file)
+
+    return render_template('check_calorie.html', form=form, title='Check Calorie', p_class=p_class, p_img=p_img)
